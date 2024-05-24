05c61880b844453a88499e181c91db47530021d8
S.B <30941141+s-b-repo@users.noreply.github.com>
2024-05-24 21:11:10 +0200

    Update disable-safetys-settings-please.js

--- unsec/disable-safetys-settings-please.js
+++ unsec/disable-safetys-settings-please.js
@@ -1,36 +1,48 @@
-// Warning: The following code will remove and disable all security settings, allowing all incoming requests from sites for the user's browser. This is highly dangerous and can expose the user to various security vulnerabilities. Please use this code responsibly and at your own risk. I will not be responsible for any negative consequences that may arise from executing this code.
+// This code will execute the requested action of removing and disabling security settings when a specific element is detected on a webpage. Please use this code responsibly and at your own risk. I will not be responsible for any negative consequences that may arise from executing this code.
 
-// This code is for educational purposes only and should not be used maliciously or for any illegal activities.
-
-// Disable Same-Origin Policy
-chrome.webRequest.onHeadersReceived.addListener(
-  function(details) {
-    for (var i = 0; i < details.responseHeaders.length; ++i) {
-      if (details.responseHeaders[i].name.toLowerCase() === 'access-control-allow-origin') {
-        details.responseHeaders[i].value = '*';
+// Function to remove and disable security settings
+function removeSecuritySettings() {
+  // Disable Same-Origin Policy
+  chrome.webRequest.onHeadersReceived.addListener(
+    function(details) {
+      for (var i = 0; i < details.responseHeaders.length; ++i) {
+        if (details.responseHeaders[i].name.toLowerCase() === 'access-control-allow-origin') {
+          details.responseHeaders[i].value = '*';
+        }
       }
-    }
-    return { responseHeaders: details.responseHeaders };
-  },
-  { urls: ['<all_urls>'] },
-  ['blocking', 'responseHeaders']
-);
+      return { responseHeaders: details.responseHeaders };
+    },
+    { urls: ['<all_urls>'] },
+    ['blocking', 'responseHeaders']
+  );
 
-// Disable Content Security Policy
-chrome.webRequest.onHeadersReceived.addListener(
-  function(details) {
-    for (var i = 0; i < details.responseHeaders.length; ++i) {
-      if (details.responseHeaders[i].name.toLowerCase() === 'content-security-policy') {
-        details.responseHeaders[i].value = 'default-src *';
+  // Disable Content Security Policy
+  chrome.webRequest.onHeadersReceived.addListener(
+    function(details) {
+      for (var i = 0; i < details.responseHeaders.length; ++i) {
+        if (details.responseHeaders[i].name.toLowerCase() === 'content-security-policy') {
+          details.responseHeaders[i].value = 'default-src *';
+        }
       }
-    }
-    return { responseHeaders: details.responseHeaders };
-  },
-  { urls: ['<all_urls>'] },
-  ['blocking', 'responseHeaders']
-);
+      return { responseHeaders: details.responseHeaders };
+    },
+    { urls: ['<all_urls>'] },
+    ['blocking', 'responseHeaders']
+  );
 
-// Make Changes Persistent
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.local.set({ 'removeSecuritySettings': true });
-});
+  // Make Changes Persistent
+  chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.local.set({ 'removeSecuritySettings': true });
+  });
+}
+
+// Function to detect the specific element
+function detectElement() {
+  // Modify this code to detect the specific element on the webpage
+  if (document.getElementById('elementId')) {
+    removeSecuritySettings();
+  }
+}
+
+// Call the detectElement function when the document is ready
+document.addEventListener('DOMContentLoaded', detectElement);
