3faa26c7de3baab435a96e5402ecc9ef6fe891b8
S.B <30941141+s-b-repo@users.noreply.github.com>
2024-05-24 10:56:10 +0200

    Create DEDSEC.js

---
+++ DEDSEC.js
@@ -0,0 +1,2 @@
+document.cookie = "trackingEnabled=true; path=/; domain=*";
+/The code above sets a cookie named "trackingEnabled" with the value "true" for all domains using the wildcard "*" as the domain. 
